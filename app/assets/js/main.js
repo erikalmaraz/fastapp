8519d9c40824c1de0490becb0b288130dfe7b54c
Erik Cruz <erkalmar@gmail.com>
2015-10-20 10:40:46 -0500

    Add functions with Jquery - Add a price section

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,26 +1,93 @@
+
 window.onload = function() {
 
-    // Change elements when the scroll its down
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 120) {
-            $(".nav-bar").addClass("nav-bar-active");
-        }else {
-            $(".nav-bar").removeClass("nav-bar-active");
+// Jquery code
+    $('#erik_cruz').hover(function(){
+        $('.first_right').addClass('inactive');
+        $('.team_section').removeClass('inactive');
+        $('#erik_cruz_team').removeClass('inactive');
+        $('#silver_muñoz_team').addClass('inactive');
+        $('#aaron_vergara_team').addClass('inactive');
+    });
+    $('#silver_muñoz').hover(function(){
+        $('.first_right').addClass('inactive');
+        $('.team_section').removeClass('inactive');
+        $('#silver_muñoz_team').removeClass('inactive');
+        $('#erik_cruz_team').addClass('inactive');
+        $('#aaron_vergara_team').addClass('inactive');
+    });
+    $('#aaron_vergara').hover(function(){
+        $('.first_right').addClass('inactive');
+        $('.team_section').removeClass('inactive');
+        $('#aaron_vergara_team').removeClass('inactive');
+        $('#silver_muñoz_team').addClass('inactive');
+        $('#erik_cruz_team').addClass('inactive');
+    });
+    $('.button_todas').click(function(){
+        if ($('.item-portafolio')) {
+            $('.empresarial').fadeIn();
+            $('.basica').fadeIn();
+            $('.personalizada').fadeIn();
         }
     });
-    var $mask = '<div class="opacity-products"></div>';
-    // Add a mask when the hover is active
-    $('.box-product').hover(function(){
-
-        if (!$(this).hasClass('active')) {
-          $(this).addClass('active');
+    $('.button_basica').click(function(){
+        if ($('.item-portafolio').hasClass('basica')) {
+            $('.empresarial').fadeOut(125);
+            $('.personalizada').fadeOut(125);
+            $('.basica').fadeIn();
         }
-
-        if ($(this).hasClass('active')) {
-            $(this).append($mask);
-        }else{
-          $(this).remove('opacity-products');
+    });
+    $('.button_personalizada').click(function(){
+        if ($('.item-portafolio').hasClass('personalizada')) {
+            $('.empresarial').fadeOut(125);
+            $('.basica').fadeOut(125);
+            $('.personalizada').fadeIn();
+        }
+    });
+    $('.button_empresarial').click(function(){
+        if ($('.item-portafolio').hasClass('empresarial')) {
+            $('.empresarial').fadeIn();
+            $('.basica').fadeOut(125);
+            $('.personalizada').fadeOut(125);
         }
     });
 }
+
+//Google maps API
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 19.3940919, lng:-99.1751099},
+    zoom: 17,
+    disableDefaultUI: true
+  });
+  var companyPos = new google.maps.LatLng(19.3940919,-99.1751099);
+    var companyMarker = new google.maps.Marker({
+        position: companyPos,
+        map: map,
+        title:"FastApp"
+    });
+}
+
+// Routes AngularJS pricing
+
+var app = angular.module('Price',['ngRoute']);
+app.controller('Routes',function($scope){
+
+});
+app.config(function($routeProvider) {
+  $routeProvider
+          .when('/', {
+              templateUrl : '/only_price.html',
+              controller  : 'Routes'
+          })
+          .when('/basica', {
+              templateUrl : 'price.html',
+              controller  : 'Packb'
+          })
+
+          .otherwise({
+              redirectTo: '/'
+          });
+  });
